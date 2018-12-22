dee77a19e6c69a32907ebf100992d3bccb7c714e
liulinming <849760808@qq.com>
2018-12-22 20:13:22 +0800

    将工程添加到本地仓库

---
+++ git-test/src/com/itheima/git/Firstclass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class Firstclass {
+    public static void main(String[] args) {
+        System.out.println("hehe");
+        System.out.println("hehe");
+        System.out.println("hehe");
+        System.out.println("hehe");
+        System.out.println("hehe");
+        System.out.println("hehe");
+    }
+}
